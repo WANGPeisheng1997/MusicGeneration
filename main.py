2a8ee4e32c9f3eb6f9caddcf2015060490265afc
王沛晟 <115681033@qq.com>
2018-03-13 18:37:03 +0800

    Two histograms added
    Add two histograms of 1000 musics and re-write main.py

--- main.py
+++ main.py
@@ -3,32 +3,46 @@ import os
 import pandas
 import matplotlib.pyplot
 
-midi_list = []
-for root, dirs, files in os.walk("./melody", topdown=False):
-    for name in files:
-        if name.endswith(".mid"):
-            midi_list.append(os.path.join(root, name))
+def generate_midi_list():
+    midi_list = []
+    for root, dirs, files in os.walk("./melody", topdown=False):
+        for name in files:
+            if name.endswith(".mid"):
+                midi_list.append(os.path.join(root, name))
+    return midi_list
 
-music_list = []
-for midi in midi_list:
-    music_list.append(midi2music(midi))
+def generate_music_list(midi_list):
+    music_list = []
+    for midi in midi_list:
+        music_list.append(midi2music(midi))
+    music_list.sort(key=lambda music: music.get_notes())
+    return music_list
 
-music_list.sort(key=lambda music: music.get_notes())
+def print_music_list(music_list):
+    for i, music in enumerate(music_list):
+        print('Music {}: Notes {}, Length {}, Name {}'.format(i + 1, music.get_notes(), music.get_lengths(), music.name))
 
-for i, music in enumerate(music_list):
-    print('Music {}: Notes {}, Length {}, Name {}'.format(i + 1, music.get_notes(), music.get_lengths(), music.name))
+def show_histogram_of_notes(music_list):
+    music_notes = []
+    for music in music_list[:-10]:
+        music_notes.append(int(music.get_notes()))
+    data = pandas.Series(music_notes)
+    data.hist(figsize=(10,6),bins=20)
+    matplotlib.pyplot.show()
 
-# music_notes = []
-# for music in music_list:
-#     if music.get_notes()<= 1000:
-#         music_notes.append(int(music.get_notes()))
+def show_histogram_of_lengths(music_list, seconds=False):
+    music_lengths = []
+    for music in music_list[:-10]:
+        if seconds:
+            music_lengths.append(int(music.get_lengths()/1000*0.67))
+        else:
+            music_lengths.append(int(music.get_lengths()))
+    data = pandas.Series(music_lengths)
+    data.hist(figsize=(10,6),bins=20)
+    matplotlib.pyplot.show()
 
-music_lengths = []
-for music in music_list[:-10]:
-    if music.get_lengths() <= 800000:
-        music_lengths.append(int(music.get_lengths()/1000*0.67))
-
-data = pandas.Series(music_lengths)
-data.hist()
-matplotlib.pyplot.show()
-#print(midi_list)
+if __name__ == '__main__':
+    midi_list = generate_midi_list()
+    music_list = generate_music_list(midi_list)
+    #show_histogram_of_lengths(music_list, seconds=True)
+    show_histogram_of_notes(music_list)
